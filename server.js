e0fd06848a253d449737ae284010fcd40f5a51a3
Anderson L Polo A <anderson@alphasystem.dev>
2025-10-09 19:51:59 -0300

    Version 0.7 Code 1

--- server.js
+++ server.js
@@ -24,6 +24,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use(async (req, res, next) => {
+  pb.authStore.clear();
+
   try {
     const authCookie = req.cookies.pb_auth;
     if (authCookie) {
@@ -37,7 +39,12 @@ app.use(async (req, res, next) => {
   res.locals.userAvatarUrl = null;
   try {
     if (res.locals.user?.avatar) {
-      res.locals.userAvatarUrl = pb.files.getURL(res.locals.user, res.locals.user.avatar, { thumb: "80x80" });
+      const internalUrl = pb.files.getURL(res.locals.user, res.locals.user.avatar, { thumb: "80x80" });
+      if (process.env.PUBLIC_POCKETBASE_URL && process.env.POCKETBASE_URL) {
+        res.locals.userAvatarUrl = internalUrl.replace(process.env.POCKETBASE_URL, process.env.PUBLIC_POCKETBASE_URL);
+      } else {
+        res.locals.userAvatarUrl = internalUrl;
+      }
     }
   } catch (e) {
     res.locals.userAvatarUrl = null;
@@ -48,6 +55,15 @@ app.use(async (req, res, next) => {
 app.use("/", dashboardRoutes);
 
 app.use(express.static(path.join(__dirname, "public")));
+
+app.use((req, res, next) => {
+  res.status(404).render("404");
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).render("500");
+});
 
 app.listen(port, () => {
   console.log(`Skopos server listening at http://localhost:${port}`);
